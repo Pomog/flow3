4995bcf05ea89aec7a8aaed07730705eb93e97aa
Yurii Panasuik <thoryur@gmail.com>
2025-05-03 21:11:05 +0300

    saveProcessToServer

--- flow-front/src/features/process/MockockApi.ts
+++ flow-front/src/features/process/MockockApi.ts
@@ -6,12 +6,11 @@ import { v4 as uuidv4 } from 'uuid';
  * Returns a promise with a new Process object including a generated `id`.
  */
 export async function saveProcessToServer(
-    partial: Omit<Process, 'id'>
-): Promise<Process> {
+    process: Process): Promise<Process> {
     return new Promise(resolve => {
         setTimeout(() => {
             resolve({
-                ...partial,
+                ...process,
                 id: uuidv4(),
             });
         }, 300);
